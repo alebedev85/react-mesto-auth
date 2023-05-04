7b2f90bc242f473782d5bd3474b6e065d5ae6374
Andrey Lebedev <alebedev424@gmail.com>
2023-05-04 15:59:54 +0300

    refactor: сделал навигацию

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,21 @@
-import { NavLink } from 'react-router-dom';
-export default function NavBar({ email, logOut, loggedIn }){
+import { NavLink, Routes, Route } from 'react-router-dom';
+
+export default function NavBar({ email, logOut, loggedIn }) {
   return (
     <nav className="navbar">
-        {loggedIn ?
-          <>
-            <p className={'navbar__text'}>{email}</p>
-            <button type="button" className={'navbar__link navbar__button'} onClick={logOut}>Выйти</button>
-          </> :
-          <>
-            <NavLink to="/" className={'navbar__link'}>Mesto</NavLink>
-            <NavLink to="/sign-in" className={'navbar__link'}>Войти</NavLink>
-            <NavLink to="/sign-up" className={'navbar__link'}>Регистрация</NavLink>
-          </>}
-      </nav>
+      {loggedIn ?
+        <>
+          <p className={'navbar__text'}>{email}</p>
+          <button type="button" className={'navbar__link navbar__button'} onClick={logOut}>Выйти</button>
+        </> :
+        <>
+          {/* <NavLink to="/" className={'navbar__link'}>Mesto</NavLink> */}
+          <Routes>
+            <Route path="*" element={<NavLink to="/sign-up" className={'navbar__link'}>Регистрация</NavLink>} />
+            <Route path='/sign-in' element={<NavLink to="/sign-up" className={'navbar__link'}>Регистрация</NavLink>} />
+            <Route path='/sign-up' element={<NavLink to="/sign-in" className={'navbar__link'}>Войти</NavLink>} />
+          </Routes>
+        </>}
+    </nav>
   )
 }
